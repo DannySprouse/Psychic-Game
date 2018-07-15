2fd02145cb6824dbc7e249cfa68ed19bd0554d8b
Danny Sprouse <dannyssprouse@gmail.com>
2018-07-15 18:30:45 -0400

    refactored code and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Psychic-Game
-A psychic game app
+
+PSYCHIC GAME
+
+Tools: HTML5, CSS3, Bootstrap4, Google Fonts, JavaScript
+
+Summary: This is a very simple JavaScript game where the computer will generate a random letter from the alphabet and the user tries to guess that letter by using the keyboard to select their letter choice. The user has 10 tries to guess the letter. If the user cannot achieve that within 10 guesses, a LOSS is scored and the computer generates a new letter for the user to try guessing again. If the user does achieve the correct guess within the allotted 10 guesses, a WIN is scored and the computer again generates a new random letter so the user can play again.
+
+Problem: The task was to create an app to run in browser that features dynamically updating HTML code, JavaScript and computer random generation. I needed code for the computer to generate a random letter, then receive user input to see if the input matches the generated letter, and score a win or a loss based on a set number (10) of tries before losing.
+
+Solution: Using JavaScript, create an array and have computer cycle through the array for a randomly selected letter, and perform matches against that letter and the user input. A variety of alerts were added to help user navigate the game.
+
+My Technical Approach: I first built out a basic HTML mockup with some simple CSS styling. I then coded out the JavaScript in the game.js file. I wanted to provide the user with an alert with very brief instructions on how to play the game, so created an info button that when clicked would provide these instructions. I defined an array with the alphabet and set various variables for wins, losses, guesses and of course the computer generated number using Math.floor(Math.random(), and cycled through the array randomly to select a letter. To avoid a situation where a user might have on CAPS lock, used .toLowerCase for the user selection to match the letters of the array. I used a for loop to loop through the guesses and determine if the guess was correct and to calculate how many guesses the user had used up. Then, if/else if was used to determine if the user and computer selections matched resulting in a win, and then else if there were more guesses and a wrong guess, user could go again, but else if there were no more guesses and a wrong guess, a loss was recorded. The guesses, wins, losses and letters guessed were all written to the HTML. Alerts guided the player with information about guessed letters, wins and losses. I then made a few additional CSS adjustments, ensured the game was mobile responsive, and deployed.
+
+Licenses, Credits & Attributions: Purchased Fortune Teller image license from DandDDigitalDelights, May 2018.
+
+Link to Deployed Game: https://dannysprouse.github.io/Psychic-Game/
+
+Copyright ©2018 Danny S. Sprouse All Rights Reserved
---
+++ assets/javascript/game.js
@@ -0,0 +1,99 @@
+    // An alert to give an overview of the game/how to play
+
+document.getElementById('info').onclick = function() {
+    alert("The computer has selected a random letter. Use your keyboard to select the letter you believe has been selected.  You get up to 10 guesses.  If you guess the letter within your 10 tries, you score 1 Win; if you do not guess after 10 tries, you get 1 Loss. Once a Win or Loss has been scored, the computer will generate a new random letter and you get another 10 tries! Good Luck!!");
+}
+   
+   // Define the array from which the computer will generate the random letter picks
+
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    // Define additional game variables
+
+    var wins = 0; // Will be an integer, score increases based on wins
+    var losses = 0; // Will be an integer, score increases based on losses
+    var guessesRemaining = 10; // Set remaining guesses to 9, for a maximum of 10 guesses
+    var guessesSoFar = []; // Create an array of guessed letters per user input
+    var userSelection = ""; // Empty string, holding user inputs
+
+    // Define how the computer will randomly select a letter from the alphabet array
+
+    var computerSelection = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    // Console to make sure a random letter is being generated
+
+        console.log(computerSelection);
+
+    // Set the function to activate game with onkeyup 
+
+document.onkeyup = function(event) { 
+
+    // Need to convert values into characters and ensure that if the CAPS LOCK is on, the input will be converted to lower case to match the array
+
+    var userSelection = String.fromCharCode(event.keyCode).toLowerCase();
+
+    // Alert the user if they have already selected that letter/duplication
+
+    var isDuplicate = false;
+        for (var i = 0; i < guessesSoFar.length; i++) 
+        {
+            if (userSelection == guessesSoFar[i])
+            {
+                isDuplicate = true;
+                    alert("You just wasted one guess.  You have used that letter already!");
+            }
+        }
+
+    // Add user input to guesses so far
+
+    guessesSoFar.push(userSelection);
+
+    // If user guesses correctly, increase wins and alert they are psychic, and reset game for next random letter
+
+    if (userSelection == computerSelection) 
+    {
+        wins++;
+        alert("You must be psychic!  Click OK and play again.");
+        guessesRemaining = 10; // Reset guesses to 10 to play again
+        guessesSoFar.length = 0;
+    
+    // After a win, computer selects a new random letter, and new generation is console logged
+
+        computerSelection = alphabet[Math.floor(Math.random() * alphabet.length)];
+            console.log(computerSelection);
+    }
+    // If user guesses incorrectly, increase losses and alert to try again, and reset game
+
+    else if (guessesRemaining == 0) 
+    {
+        losses++;
+        alert("Maybe you\'re not psychic!  Click OK and try again!");
+        guessesRemaining = 10; // Reset guesses to 10 to play again
+        guessesSoFar.length = 0;
+    
+    // After a loss, computer selects a new random letter, and new generation is console logged
+
+        computerSelection = alphabet[Math.floor(Math.random() * alphabet.length)];
+            console.log(computerSelection);
+    }
+
+    // As user continues to make guesses, when incorrect decrease remaining available guesses
+
+    else if (userSelection !== computerSelection)
+    {
+        guessesRemaining--;
+    }
+ 
+    // Write results to the page and update HTML as user plays
+
+    var html = 
+        "<p>Wins: " +  wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses Remaining: " + guessesRemaining + "</p>" +
+        "<p>Your Guesses So Far: " + guessesSoFar + "</p>";
+
+    // Return the element that matches the CSS selector ID #game and then change the text of that element using innerHTML
+
+    document.querySelector('#game').innerHTML = html;
+
+} // This closes the onkeyup function
